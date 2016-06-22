1fb6c824c593a0c52d8ea368c7235d1107495a40
Jorge Gomez <jorgemario@belucasys.com>
2016-06-21 23:46:35 -0500

    Adding Scala support

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
+  
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
